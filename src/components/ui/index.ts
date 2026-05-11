e2a2e74002df9ce2aa01f6f0935b759f23e5ac30
chacha091 <chhyju09@gmail.com>
2026-05-11 15:56:24 +0900

    chore(project): 미사용 업로드 컴포넌트 제거

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -4,5 +4,4 @@ export { default as Modal } from './Modal';
 export { default as FileThumb } from './FileThumb';
 export { default as InlineLoader } from './InlineLoader';
 export { default as CenterModal } from './CenterModal';
-export { default as FolderIcon } from './FolderIcon';
 export { default as ChevronIcon } from './ChevronIcon';
